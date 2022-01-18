bd57c3602ec94af1af21a7fdd09cb685544e0332
krrong <khk9664@naver.com>
2022-01-18 23:24:03 +0900

    modify

--- app/src/main/java/com/example/convenience_stores/MainActivity.java
+++ app/src/main/java/com/example/convenience_stores/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.convenience_stores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button cu_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cu_btn = findViewById(R.id.cu_btn);
+
+        cu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), cu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
