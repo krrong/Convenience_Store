30c1ec3babc8e495fff9eecbca4de5a0d03a9c29
krrong <khk9664@naver.com>
2022-01-19 19:40:33 +0900

    listview에 담을 item 객체

---
+++ app/src/main/java/com/example/convenience_stores/singleItem.java
@@ -0,0 +1,33 @@
+package com.example.convenience_stores;
+
+public class singleItem {
+    String name;
+    String price;
+    int resId;
+
+    // 생성자
+    public singleItem(String name, String price, int resId){
+        this.name = name;
+        this.price = price;
+        this.resId = resId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPrice(String price){
+        this.price = price;
+    }
+    public void setResId(int resId){
+        this.resId = resId;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getPrice(){
+        return this.price;
+    }
+    public int getResId(){
+        return this.resId;
+    }
+}
