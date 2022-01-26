a99f470d6a30dc9dd9ad6bcce1e40541d3f38dc5
krrong <khk9664@naver.com>
2022-01-27 01:01:45 +0900

    주석으로 설명 추가

--- app/src/main/java/com/example/convenience_stores/singleItemAdapter.java
+++ app/src/main/java/com/example/convenience_stores/singleItemAdapter.java
@@ -14,31 +14,38 @@ public class singleItemAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<singleItem> array_singleItem = new ArrayList<>();
 
+    // 생성자
     public singleItemAdapter(ArrayList<singleItem> array_singleItem, Context context){
         this.context = context;
         this.array_singleItem = array_singleItem;
     }
 
+    // 데이터 개수 리턴
     @Override
     public int getCount() {
         return array_singleItem.size();
     }
 
+    // position 위치의 아이템 리턴
     @Override
     public Object getItem(int position) {
         return array_singleItem.get(position);
     }
 
+    // position 위치 아이템의 id 리턴
     @Override
     public long getItemId(int position) {
         return position;
     }
 
+    // i번째 아이템을 어떻게 보여줄 것인지 결정
     @Override
     public View getView(int position, View view, ViewGroup parent) {
+        // LayoutInflater를 통해 layout파일 메모리에 객체화
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = inflater.inflate(R.layout.item_list, parent, false);
 
+        // 각 view의 내용 설정
         TextView name = view.findViewById(R.id.text_name);
         name.setText(array_singleItem.get(position).getName());
 
@@ -47,7 +54,8 @@ public class singleItemAdapter extends BaseAdapter {
 
         ImageView image = view.findViewById(R.id.imageView);
         image.setImageResource(array_singleItem.get(position).getResId());
-
+        
+        // view 반환
         return view;
     }
 
