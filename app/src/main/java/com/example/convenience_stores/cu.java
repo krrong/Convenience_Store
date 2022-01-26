bedd77e65a38afb7ef61a395b4172b2eaabc3a10
krrong <khk9664@naver.com>
2022-01-27 00:26:42 +0900

    change adapter

--- app/src/main/java/com/example/convenience_stores/cu.java
+++ app/src/main/java/com/example/convenience_stores/cu.java
@@ -10,7 +10,9 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class cu extends AppCompatActivity {
     Button rtn_btn;
@@ -29,52 +31,10 @@ public class cu extends AppCompatActivity {
         });
 
         ListView listView = findViewById(R.id.cu_listview);
-        SingleAdapter adapter = new SingleAdapter();
-        adapter.addItem(new singleItem("롯데)빅팜60g","1600원",R.drawable.img0));
-        adapter.addItem(new singleItem("해피바스)로즈바디워시","5000원",R.drawable.img1));
-        adapter.addItem(new singleItem("46cm)초극세모칫솔","3500원",R.drawable.img2));
+        ArrayList<singleItem> items = new ArrayList<>();
+        items.add(new singleItem("빅팜","1600원",R.drawable.img0));
 
+        singleItemAdapter adapter = new singleItemAdapter(items, getApplicationContext());
         listView.setAdapter(adapter);
-    }
-
-    class SingleAdapter extends BaseAdapter{
-        ArrayList<singleItem> items = new ArrayList<singleItem>();
-
-        @Override
-        public int getCount(){
-            return items.size();
-        }
-
-        public void addItem(singleItem item){
-            items.add(item);
-        }
-
-        @Override
-        public Object getItem(int position){
-            return items.get(position);
-        }
-
-        @Override
-        public long getItemId(int position){
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent){
-            singleItemView singleItemView1 = null;
-
-            if(convertView == null){
-                singleItemView1 = new singleItemView(getApplicationContext());
-            }
-            else{
-                singleItemView1 = (singleItemView)convertView;
-            }
-            singleItem item = items.get(position);
-            singleItemView1.setName(item.getName());
-            singleItemView1.setPrice(item.getPrice());
-            singleItemView1.setImageView(item.getResId());
-
-            return singleItemView1;
-        }
     }
 }
