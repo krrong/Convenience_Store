1ffef0ad913d4b081607b9d5e69da82add8f7786
krrong <khk9664@naver.com>
2022-02-07 16:55:24 +0900

    url 파일 사용

--- app/src/main/java/com/example/convenience_stores/cu.java
+++ app/src/main/java/com/example/convenience_stores/cu.java
@@ -2,6 +2,7 @@ package com.example.convenience_stores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -27,6 +28,7 @@ public class cu extends AppCompatActivity {
         setContentView(R.layout.activity_cu);
         String[] CU_name;
         String[] CU_price;
+        String[] CU_url;
 
         // 돌아가기 버튼 + 기능
         rtn_btn = findViewById(R.id.rtn_btn);
@@ -41,36 +43,53 @@ public class cu extends AppCompatActivity {
         try {
             InputStream in_name = getResources().openRawResource(R.raw.cu_11_name);
             InputStream in_price = getResources().openRawResource(R.raw.cu_11_price);
+            InputStream in_url  = getResources().openRawResource(R.raw.cu_11_link);
 
             byte[] b_name = new byte[in_name.available()];      // available = 읽을 수 있는 바이트 수 반환
             byte[] b_price = new byte[in_price.available()];    // available = 읽을 수 있는 바이트 수 반환
+            byte[] b_url  = new byte[in_url.available()];    // available = 읽을 수 있는 바이트 수 반환
 
             // 인자만큼 읽어오기
             in_name.read(b_name);
             in_price.read(b_price);
+            in_url.read(b_url);
 
             // byte -> string 변환
             String s_name = new String(b_name);
             String s_price = new String(b_price);
+            String s_url = new String(b_url);
 
-//            Log.e("test",s_name);       // 잘 읽어오는지 확인을 위한 로그
-//            Log.e("test",s_price);      // 잘 읽어오는지 확인을 위한 로그
+//            Log.e("test",s_name)          // 잘 읽어오는지 확인을 위한 로그
+//            Log.e("test",s_price);        // 잘 읽어오는지 확인을 위한 로그
+//            Log.e("test",s_url);          // 잘 읽어오는지 확인을 위한 로그
 
-            // "\n" 단위로 나누어 각각 배열에 저장
+            // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
             CU_name = s_name.split("\n");
             CU_price = s_price.split("\n");
+            CU_url = s_url.split("\n");
+
+
+
+            ListView listView = findViewById(R.id.cu_listview);                     // listview
+            ArrayList<singleItem> items = new ArrayList<>();                        // item 저장할 ArrayList
+
+            for(int i=0; i<CU_name.length; i++){
+                items.add(new singleItem(CU_name[i], CU_price[i], CU_url[i]));
+            }
+
+
+            singleItemAdapter adapter = new singleItemAdapter(items, getApplicationContext());  // 어댑터 생성
+            listView.setAdapter(adapter);   // 어댑터 연결
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        ListView listView = findViewById(R.id.cu_listview);                     // listview
-        ArrayList<singleItem> items = new ArrayList<>();                        // item 저장할 ArrayList
-        items.add(new singleItem("롯데)빅팜60g","1600원",R.drawable.img0));          // item 추가
-        items.add(new singleItem("해피바스)로즈바디워시","5000원",R.drawable.img1));   // item 추가
-        items.add(new singleItem("46cm)초극세모칫솔","3500원",R.drawable.img2));     // item 추가
 
-        singleItemAdapter adapter = new singleItemAdapter(items, getApplicationContext());  // 어댑터 생성
-        listView.setAdapter(adapter);   // 어댑터 연결
+//        items.add(new singleItem("롯데)빅팜60g","1600원",R.drawable.img0));          // item 추가
+//        items.add(new singleItem("해피바스)로즈바디워시","5000원",R.drawable.img1));   // item 추가
+//        items.add(new singleItem("46cm)초극세모칫솔","3500원",R.drawable.img2));     // item 추가
+
+
     }
 }
