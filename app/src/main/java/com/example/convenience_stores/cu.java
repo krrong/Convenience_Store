8b7f1f7c71b6f64687418b7b4601baef60e53329
krrong <khk9664@naver.com>
2022-02-07 15:24:08 +0900

    txt파일로부터 상품 이름 가격 읽어오기

--- app/src/main/java/com/example/convenience_stores/cu.java
+++ app/src/main/java/com/example/convenience_stores/cu.java
@@ -3,6 +3,7 @@ package com.example.convenience_stores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -10,6 +11,9 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +25,10 @@ public class cu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cu);
+        String[] CU_name;
+        String[] CU_price;
 
+        // 돌아가기 버튼 + 기능
         rtn_btn = findViewById(R.id.rtn_btn);
         rtn_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +36,34 @@ public class cu extends AppCompatActivity {
                 finish();
             }
         });
-        
-        
+
+        // CU 1+1 상품, 가격 파일로부터 읽어오기
+        try {
+            InputStream in_name = getResources().openRawResource(R.raw.cu_11_name);
+            InputStream in_price = getResources().openRawResource(R.raw.cu_11_price);
+
+            byte[] b_name = new byte[in_name.available()];      // available = 읽을 수 있는 바이트 수 반환
+            byte[] b_price = new byte[in_price.available()];    // available = 읽을 수 있는 바이트 수 반환
+
+            // 인자만큼 읽어오기
+            in_name.read(b_name);
+            in_price.read(b_price);
+
+            // byte -> string 변환
+            String s_name = new String(b_name);
+            String s_price = new String(b_price);
+
+//            Log.e("test",s_name);       // 잘 읽어오는지 확인을 위한 로그
+//            Log.e("test",s_price);      // 잘 읽어오는지 확인을 위한 로그
+
+            // "\n" 단위로 나누어 각각 배열에 저장
+            CU_name = s_name.split("\n");
+            CU_price = s_price.split("\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         ListView listView = findViewById(R.id.cu_listview);                     // listview
         ArrayList<singleItem> items = new ArrayList<>();                        // item 저장할 ArrayList
         items.add(new singleItem("롯데)빅팜60g","1600원",R.drawable.img0));          // item 추가
