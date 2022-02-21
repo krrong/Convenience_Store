593028b769d7fe54a575e2763cd0cd435ef9c6f6
krrong <khk9664@naver.com>
2022-02-21 18:02:08 +0900

    Glide를 사용하여 이미지 받아오는 속도 향상
    기존 방법은 HttpURLConnection과 InputStream을 이용해서 url로부터 이미지를 받아오는 방법이었고, 속도가 매우 느려 리사이클러뷰가 버벅거리는 문제가 있었다.
    
    해결하기 위해 Glide를 찾았고, gradle에 dependencies를 추가하고 사용할 수 있다.
    
    현재 Glide를 이용하여 조금 더 빠른 속도로 URL로부터 이미지를 받아오지만 URL을 사용하는 함수에 줄이 그어져 있는 것으로 보아 다른 방법을 찾아보아야 할 듯 하다.

--- app/src/main/java/com/example/convenience_stores/ItemAdapter.java
+++ app/src/main/java/com/example/convenience_stores/ItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.convenience_stores;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
@@ -13,9 +14,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+//import com.bumptech.glide.Glide;
+
+import com.bumptech.glide.Glide;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -23,6 +29,7 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final int VIEW_TYPE_ITEM = 0;
     private final int VIEW_TYPE_LOADING = 1;
 
+//    private Context context;
     private ArrayList<singleItem> items;    // 어댑터에 들어갈 list
     Bitmap bitmap;  // 이미지 비트맵
 
@@ -46,38 +53,52 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             name.setText(item.getName());
             price.setText(item.getPrice());
 
-            // 안드로이드에서 네트워크와 관련된 작업을 할 때 별도의 작업 Thread를 생성하여 작업해야 한다.
-            Thread uThread = new Thread(){
-                @Override
-                public void run() {
-                    try{
-                        // 객체에 저장된 url을 가지고 온다.
-                        URL url = new URL(item.getUrl());
-                        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 
-                        conn.setDoInput(true);  // 서버통신에서 입력 가능한 상태로 만든다.
-                        conn.connect();         // 연결된 곳에 접속
-
-                        InputStream is = conn.getInputStream();     // inputStream 값 가져오기
-                        bitmap = BitmapFactory.decodeStream(is);    // bitmap으로 변환
-                    }catch(IOException e){
-                        e.printStackTrace();
-                    }
-
-                }
-            };
-            // 작업 Thread 시작
-            uThread.start();
-
-            try{
-                // join() : 별도의 작업 Thread가 종료될 때까지 메인 Thread 대기시킴
-                // -> InterruptException 발생시킴
-
-                uThread.join();
-                imageView.setImageBitmap(bitmap);
-            }catch(InterruptedException e){
+            URL imageUrl = null;
+            try {
+                imageUrl = new URL(item.getUrl());
+            } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
+
+            Glide
+                    .with(imageView.getContext())
+                    .load(imageUrl)
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(imageView);
+
+//            // 안드로이드에서 네트워크와 관련된 작업을 할 때 별도의 작업 Thread를 생성하여 작업해야 한다.
+//            Thread uThread = new Thread(){
+//                @Override
+//                public void run() {
+//                    try{
+//                        // 객체에 저장된 url을 가지고 온다.
+//                        URL url = new URL(item.getUrl());
+//                        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+//
+//                        conn.setDoInput(true);  // 서버통신에서 입력 가능한 상태로 만든다.
+//                        conn.connect();         // 연결된 곳에 접속
+//
+//                        InputStream is = conn.getInputStream();     // inputStream 값 가져오기
+//                        bitmap = BitmapFactory.decodeStream(is);    // bitmap으로 변환
+//                    }catch(IOException e){
+//                        e.printStackTrace();
+//                    }
+//
+//                }
+//            };
+//            // 작업 Thread 시작
+//            uThread.start();
+//
+//            try{
+//                // join() : 별도의 작업 Thread가 종료될 때까지 메인 Thread 대기시킴
+//                // -> InterruptException 발생시킴
+//
+//                uThread.join();
+//                imageView.setImageBitmap(bitmap);
+//            }catch(InterruptedException e){
+//                e.printStackTrace();
+//            }
         }
     }
 
@@ -93,7 +114,7 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     // 생성자
     ItemAdapter(ArrayList<singleItem> list) {
-        items = list;
+        this.items = list;
     }
 
     @Override
