3adfffe3eab3af6c98defc7a17d0c56aa02fbd82
krrong <khk9664@naver.com>
2022-03-01 17:10:11 +0900

    cu, 세븐일레븐 gs25 1+1, 2+1 행사 모두 가능하도록 수정
    이전 화면에서 Intent를 넘겨줄 때 선택한 편의점 정보와 행사 정보를 같이 넘겨주면서 같은 액티비티에서 다른 화면을 보여주도록 구성했다.

--- app/src/main/java/com/example/convenience_stores/cu.java
+++ app/src/main/java/com/example/convenience_stores/cu.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
@@ -43,20 +44,42 @@ public class cu extends AppCompatActivity {
     String[] CU_price;  // 가격 배열
     String[] CU_url;    // 링크 배열
 
+    String[] SEVEN_name;   // 이름 배열
+    String[] SEVEN_price;  // 가격 배열
+    String[] SEVEN_url;    // 링크 배열
+
+    String[] GS_name;   // 이름 배열
+    String[] GS_price;  // 가격 배열
+    String[] GS_url;    // 링크 배열
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cu);
 
+        Intent intent = getIntent();
+        String place = intent.getStringExtra("place");
+        String event = intent.getStringExtra("event");
+
+        if (place.equals("cu")){
+            cuDataParsing(event);
+        }
+
+        else if (place.equals("seven")){
+            sevenDataParsing(event);
+        }
+        else if(place.equals("gs")){
+            gsDataParsing(event);
+        }
+
         initReturnBtn();
-        dataParsing();
-        dataLoad();
+        dataLoad(place);
         initAdapter();
-        initScrollListener();
+        initScrollListener(place);
         initEditText();
     }
 
-    // 돌아가기 버튼+기능
+    // 돌아가기 버튼 + 기능
     private void initReturnBtn(){
         rtn_btn = findViewById(R.id.rtn_btn);
         rtn_btn.setOnClickListener(new View.OnClickListener() {
@@ -68,46 +91,211 @@ public class cu extends AppCompatActivity {
     }
 
     // 상품, 가격, 링크를 [res] -> [raw] -> [.txt]파일로부터 읽어와 배열에 저장
-    private void dataParsing(){
-        try {
-            InputStream inName = getResources().openRawResource(R.raw.cu_11_name);
-            InputStream inPrice = getResources().openRawResource(R.raw.cu_11_price);
-            InputStream inUrl  = getResources().openRawResource(R.raw.cu_11_link);
+    private void cuDataParsing(String event){
+        if (event.equals("11")){
+            try {
+                InputStream inName = getResources().openRawResource(R.raw.cu_11_name);
+                InputStream inPrice = getResources().openRawResource(R.raw.cu_11_price);
+                InputStream inUrl  = getResources().openRawResource(R.raw.cu_11_link);
 
-            byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
-            byte[] bPrice = new byte[inPrice.available()];
-            byte[] bUrl  = new byte[inUrl.available()];
+                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+                byte[] bPrice = new byte[inPrice.available()];
+                byte[] bUrl  = new byte[inUrl.available()];
 
-            // 인자만큼 읽어오기
-            inName.read(bName);
-            inPrice.read(bPrice);
-            inUrl.read(bUrl);
+                // 인자만큼 읽어오기
+                inName.read(bName);
+                inPrice.read(bPrice);
+                inUrl.read(bUrl);
 
-            // byte -> string 변환
-            String s_name = new String(bName);
-            String s_price = new String(bPrice);
-            String s_url = new String(bUrl);
+                // byte -> string 변환
+                String s_name = new String(bName);
+                String s_price = new String(bPrice);
+                String s_url = new String(bUrl);
 
 ////            잘 읽어오는지 확인을 위한 로그
 //            Log.e("test",s_name);
 //            Log.e("test",s_price);
 //            Log.e("test",s_url);
 
-            // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
-            CU_name = s_name.split("\n");
-            CU_price = s_price.split("\n");
-            CU_url = s_url.split("\n");
-        } catch (IOException e) {
-            e.printStackTrace();
+                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+                CU_name = s_name.split("\n");
+                CU_price = s_price.split("\n");
+                CU_url = s_url.split("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try {
+                InputStream inName = getResources().openRawResource(R.raw.cu_21_name);
+                InputStream inPrice = getResources().openRawResource(R.raw.cu_21_price);
+                InputStream inUrl  = getResources().openRawResource(R.raw.cu_21_link);
+
+                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+                byte[] bPrice = new byte[inPrice.available()];
+                byte[] bUrl  = new byte[inUrl.available()];
+
+                // 인자만큼 읽어오기
+                inName.read(bName);
+                inPrice.read(bPrice);
+                inUrl.read(bUrl);
+
+                // byte -> string 변환
+                String s_name = new String(bName);
+                String s_price = new String(bPrice);
+                String s_url = new String(bUrl);
+
+                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+                CU_name = s_name.split("\n");
+                CU_price = s_price.split("\n");
+                CU_url = s_url.split("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 상품, 가격, 링크를 [res] -> [raw] -> [.txt]파일로부터 읽어와 배열에 저장
+    private void sevenDataParsing(String event){
+        if (event.equals("11")){
+            try {
+                InputStream inName = getResources().openRawResource(R.raw.seven_11_name);
+                InputStream inPrice = getResources().openRawResource(R.raw.seven_11_price);
+                InputStream inUrl  = getResources().openRawResource(R.raw.seven_11_link);
+
+                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+                byte[] bPrice = new byte[inPrice.available()];
+                byte[] bUrl  = new byte[inUrl.available()];
+
+                // 인자만큼 읽어오기
+                inName.read(bName);
+                inPrice.read(bPrice);
+                inUrl.read(bUrl);
+
+                // byte -> string 변환
+                String s_name = new String(bName);
+                String s_price = new String(bPrice);
+                String s_url = new String(bUrl);
+
+                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+                SEVEN_name = s_name.split("\n");
+                SEVEN_price = s_price.split("\n");
+                SEVEN_url = s_url.split("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try {
+                InputStream inName = getResources().openRawResource(R.raw.seven_21_name);
+                InputStream inPrice = getResources().openRawResource(R.raw.seven_21_price);
+                InputStream inUrl  = getResources().openRawResource(R.raw.seven_21_link);
+
+                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+                byte[] bPrice = new byte[inPrice.available()];
+                byte[] bUrl  = new byte[inUrl.available()];
+
+                // 인자만큼 읽어오기
+                inName.read(bName);
+                inPrice.read(bPrice);
+                inUrl.read(bUrl);
+
+                // byte -> string 변환
+                String s_name = new String(bName);
+                String s_price = new String(bPrice);
+                String s_url = new String(bUrl);
+
+                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+                SEVEN_name = s_name.split("\n");
+                SEVEN_price = s_price.split("\n");
+                SEVEN_url = s_url.split("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 상품, 가격, 링크를 [res] -> [raw] -> [.txt]파일로부터 읽어와 배열에 저장
+    private void gsDataParsing(String event){
+        if (event.equals("11")){
+            try {
+                InputStream inName = getResources().openRawResource(R.raw.gs_11_name);
+                InputStream inPrice = getResources().openRawResource(R.raw.gs_11_price);
+                InputStream inUrl  = getResources().openRawResource(R.raw.gs_11_link);
+
+                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+                byte[] bPrice = new byte[inPrice.available()];
+                byte[] bUrl  = new byte[inUrl.available()];
+
+                // 인자만큼 읽어오기
+                inName.read(bName);
+                inPrice.read(bPrice);
+                inUrl.read(bUrl);
+
+                // byte -> string 변환
+                String s_name = new String(bName);
+                String s_price = new String(bPrice);
+                String s_url = new String(bUrl);
+
+                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+                GS_name = s_name.split("\n");
+                GS_price = s_price.split("\n");
+                GS_url = s_url.split("\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+//            try {
+//                InputStream inName = getResources().openRawResource(R.raw.gs_21_name);
+//                InputStream inPrice = getResources().openRawResource(R.raw.gs_21_price);
+//                InputStream inUrl  = getResources().openRawResource(R.raw.gs_21_link);
+//
+//                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+//                byte[] bPrice = new byte[inPrice.available()];
+//                byte[] bUrl  = new byte[inUrl.available()];
+//
+//                // 인자만큼 읽어오기
+//                inName.read(bName);
+//                inPrice.read(bPrice);
+//                inUrl.read(bUrl);
+//
+//                // byte -> string 변환
+//                String s_name = new String(bName);
+//                String s_price = new String(bPrice);
+//                String s_url = new String(bUrl);
+//
+//                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+//                GS_name = s_name.split("\n");
+//                GS_price = s_price.split("\n");
+//                GS_url = s_url.split("\n");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 
     // 처음 실행하는 데이터 로드 함수(loadLength)개의 데이터 로드
-    private void dataLoad(){
-        // ArrayList에 아이템 추가(이름, 가격, url) 290개 추가
-        for(int i=0; i<loadLength; i++){
-            adapter.addItem(new singleItem(CU_name[i], CU_price[i], CU_url[i]));
+    private void dataLoad(String place){
+        if (place.equals("cu")){
+            // ArrayList에 아이템(이름, 가격, url) loadLength(20)개 추가
+            for(int i=0; i<loadLength; i++){
+                adapter.addItem(new singleItem(CU_name[i], CU_price[i], CU_url[i]));
+            }
         }
+        else if(place.equals("seven")){
+            // ArrayList에 아이템(이름, 가격, url) loadLength(20)개 추가
+            for(int i=0; i<loadLength; i++){
+                adapter.addItem(new singleItem(SEVEN_name[i], SEVEN_price[i], SEVEN_url[i]));
+            }
+        }
+        else if(place.equals("gs")){
+            // ArrayList에 아이템(이름, 가격, url) loadLength(20)개 추가
+            for(int i=0; i<loadLength; i++){
+                adapter.addItem(new singleItem(GS_name[i], GS_price[i], GS_url[i]));
+            }
+        }
+
         // 현재 리스트 임시 저장(필터링 시 재사용 위함)
         originalList = adapter.getItems();
     }
@@ -163,7 +351,7 @@ public class cu extends AppCompatActivity {
     }
 
     // 스크롤 관련 메서드 정리
-    private void initScrollListener(){
+    private void initScrollListener(String place){
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             // 사용자가 스크롤을 움직이는 동안 반복하여 호출됨
@@ -176,11 +364,11 @@ public class cu extends AppCompatActivity {
                 super.onScrollStateChanged(recyclerView, newState);
                 // 스크롤이 최상단일 경우
                 if(!recyclerView.canScrollVertically(-1)){
-                    Log.e("cu 액티비티", "최상단");
+                    Log.e("메세지", "최상단");
                 }
                 // 스크롤이 최하단일 경우
                 else if(!recyclerView.canScrollVertically(1)){
-                    Log.e("cu 액티비티", "최하단");
+                    Log.e("메세지", "최하단");
                     System.out.println(adapter.getItemCount());
 
                     LinearLayoutManager layoutManager = (LinearLayoutManager)recyclerView.getLayoutManager();
@@ -191,7 +379,7 @@ public class cu extends AppCompatActivity {
                     if(!isLoading){
                         // 배열의 마지막이면 loadMore 함수를 이용하여 추가로드
                         if(layoutManager != null && layoutManager.findLastVisibleItemPosition() == adapter.getItemCount() -1){
-                            loadMore();
+                            loadMore(place);
                             isLoading = true;   // 2초후에 실행되는 loadMore에서 false로 바꿔줌
                         }
                     }
@@ -201,7 +389,7 @@ public class cu extends AppCompatActivity {
     }
 
 //     추가로 로드하는 함수
-    private void loadMore(){
+    private void loadMore(String place){
 //        // progressBar 보기 위한 null 데이터 추가
         adapter.addItem(null);
         adapter.notifyItemInserted(adapter.getItemCount());
@@ -217,16 +405,43 @@ public class cu extends AppCompatActivity {
                 int currentSize = scrollPosition;
                 int nextLimit = currentSize + loadLength;
 
-                // nextLimit이 전체 상품 개수보다 크면 수동으로 크기 설정
-                if(nextLimit > CU_name.length - 1){
-                    nextLimit = CU_name.length - 1;
+                if(place.equals("cu")){
+                    // nextLimit이 전체 상품 개수보다 크면 수동으로 크기 설정
+                    if(nextLimit > CU_name.length - 1){
+                        nextLimit = CU_name.length - 1;
+                    }
+
+                    // 아이템 추가
+                    while(currentSize - 1 < nextLimit){
+                        adapter.addItem(new singleItem(CU_name[currentSize], CU_price[currentSize], CU_url[currentSize]));
+                        currentSize++;
+                    }
+                }
+                else if(place.equals("seven")){
+                    // nextLimit이 전체 상품 개수보다 크면 수동으로 크기 설정
+                    if(nextLimit > SEVEN_name.length - 1){
+                        nextLimit = SEVEN_name.length - 1;
+                    }
+
+                    // 아이템 추가
+                    while(currentSize - 1 < nextLimit){
+                        adapter.addItem(new singleItem(SEVEN_name[currentSize], SEVEN_price[currentSize], SEVEN_url[currentSize]));
+                        currentSize++;
+                    }
+                }
+                else if(place.equals("gs")){
+                    // nextLimit이 전체 상품 개수보다 크면 수동으로 크기 설정
+                    if(nextLimit > GS_name.length - 1){
+                        nextLimit = GS_name.length - 1;
+                    }
+
+                    // 아이템 추가
+                    while(currentSize - 1 < nextLimit){
+                        adapter.addItem(new singleItem(GS_name[currentSize], GS_price[currentSize], GS_url[currentSize]));
+                        currentSize++;
+                    }
                 }
 
-                // 아이템 추가
-                while(currentSize - 1 < nextLimit){
-                    adapter.addItem(new singleItem(CU_name[currentSize], CU_price[currentSize], CU_url[currentSize]));
-                    currentSize++;
-                }
                 adapter.notifyDataSetChanged();
                 isLoading = false;
 
