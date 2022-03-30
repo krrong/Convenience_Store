d3b4a751756085f6ec8cbcebdc9acdd0c3154691
krrong <khk9664@naver.com>
2022-03-30 19:47:54 +0900

    탭 레이아웃, 뷰페이저 정상동작

--- app/src/main/java/com/example/convenience_stores/test.java
+++ app/src/main/java/com/example/convenience_stores/test.java
@@ -1,5 +1,6 @@
 package com.example.convenience_stores;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -10,8 +11,12 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.ClipData;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
@@ -34,11 +39,25 @@ public class test extends FragmentActivity {
 
     void initView(){
         viewPager2 = (ViewPager2) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         adapter = new FragmentAdapter(this);
+
         fragments.add(new onePlusOneFragment());
         fragments.add(new twoPlusOneFragment());
 
+        tabTitles.add("1+1");
+        tabTitles.add("2+1");
+
         adapter.fragmentArrayList = fragments;
+
         viewPager2.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull @NotNull TabLayout.Tab tab, int position) {
+                TextView textView = new TextView(test.this);
+                textView.setText(tabTitles.get(position));
+                tab.setCustomView(textView);
+            }
+        }).attach();
     }
 }
