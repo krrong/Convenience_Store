05f784f6281e9a29a7b6394092422c8cb198702b
krrong <khk9664@naver.com>
2022-03-31 00:20:23 +0900

    intent를 통해 데이터를 넘겨줌
    intent를 통해 상품의 데이터를 넘겨준다.

--- app/src/main/java/com/example/convenience_stores/MainActivity.java
+++ app/src/main/java/com/example/convenience_stores/MainActivity.java
@@ -29,9 +29,13 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
-    Button cu_btn;
-    Button seven_btn;
-    Button gs_btn;
+    private Button cu_btn;
+    private Button seven_btn;
+    private Button gs_btn;
+
+    private String[] nameList;
+    private String[] priceList;
+    private String[] urlList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +57,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Goods.class);
+                dataParsing("CU");
                 intent.putExtra("place","CU");
+                intent.putExtra("nameList", nameList);
+                intent.putExtra("priceList", priceList);
+                intent.putExtra("urlList", urlList);
                 startActivity(intent);
             }
         });
@@ -62,7 +70,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Goods.class);
+                dataParsing("7ELEVEn");
                 intent.putExtra("place","7ELEVEn");
+                intent.putExtra("nameList", nameList);
+                intent.putExtra("priceList", priceList);
+                intent.putExtra("urlList", urlList);
                 startActivity(intent);
             }
         });
@@ -71,7 +83,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Goods.class);
+                dataParsing("GS25");
                 intent.putExtra("place","GS25");
+                intent.putExtra("nameList", nameList);
+                intent.putExtra("priceList", priceList);
+                intent.putExtra("urlList", urlList);
                 startActivity(intent);
             }
         });
@@ -98,6 +114,55 @@ public class MainActivity extends AppCompatActivity {
             }catch(NoSuchAlgorithmException e){
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void dataParsing(String place){
+        InputStream inName;
+        InputStream inPrice;
+        InputStream inUrl;
+
+        // 편의점과 행사에 맞는 파일 가져오기
+        // 1+1
+        if (place.equals("CU")){
+            inName = getResources().openRawResource(R.raw.cu_11_name);
+            inPrice = getResources().openRawResource(R.raw.cu_11_price);
+            inUrl  = getResources().openRawResource(R.raw.cu_11_link);
+        }
+        else if(place.equals("7ELEVEn")){
+            inName = getResources().openRawResource(R.raw.seven_11_name);
+            inPrice = getResources().openRawResource(R.raw.seven_11_price);
+            inUrl  = getResources().openRawResource(R.raw.seven_11_link);
+        }
+        else{
+            inName = getResources().openRawResource(R.raw.gs_11_name);
+            inPrice = getResources().openRawResource(R.raw.gs_11_price);
+            inUrl  = getResources().openRawResource(R.raw.gs_11_link);
+        }
+
+        // 파일에서 데이터 읽어오기
+        try {
+            byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+            byte[] bPrice = new byte[inPrice.available()];
+            byte[] bUrl = new byte[inUrl.available()];
+
+            // 인자만큼 읽어오기
+            inName.read(bName);
+            inPrice.read(bPrice);
+            inUrl.read(bUrl);
+
+            // byte -> string 변환
+            String s_name = new String(bName);
+            String s_price = new String(bPrice);
+            String s_url = new String(bUrl);
+
+            // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+            nameList = s_name.split("\n");
+            priceList = s_price.split("\n");
+            urlList = s_url.split("\n");
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
