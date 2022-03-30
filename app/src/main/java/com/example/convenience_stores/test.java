af61d23dccfd4383e2db4bbb9e9f036ac1a67483
krrong <khk9664@naver.com>
2022-03-30 18:15:27 +0900

    프래그먼트 추가

--- app/src/main/java/com/example/convenience_stores/test.java
+++ app/src/main/java/com/example/convenience_stores/test.java
@@ -1,48 +1,44 @@
 package com.example.convenience_stores;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.ClipData;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 
-public class test extends AppCompatActivity {
+public class test extends FragmentActivity {
+
+    ViewPager2 viewPager2;
+    TabLayout tabLayout;
+    FragmentAdapter adapter;
+    ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+    ArrayList<String> tabTitles = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        ArrayList<singleItem> items = new ArrayList<>();
-        items.add(new singleItem("a","1000","http://bgf-cu.xcache.kinxcdn.com/product/8801051009033.jpg"));
-        items.add(new singleItem("b","1000","http://bgf-cu.xcache.kinxcdn.com/product/8801051009033.jpg"));
-        items.add(new singleItem("c","1000","http://bgf-cu.xcache.kinxcdn.com/product/8801051009033.jpg"));
-        items.add(new singleItem("d","1000","http://bgf-cu.xcache.kinxcdn.com/product/8801051009033.jpg"));
-        items.add(new singleItem("e","1000","http://bgf-cu.xcache.kinxcdn.com/product/8801051009033.jpg"));
-        items.add(new singleItem("f","1000",""));
-        items.add(new singleItem("g","1000",""));
-        items.add(new singleItem("h","1000",""));
-        items.add(new singleItem("i","1000",""));
-        items.add(new singleItem("j","1000",""));
-        items.add(new singleItem("k","1000",""));
-        items.add(new singleItem("l","1000",""));
-        items.add(new singleItem("m","1000",""));
-        items.add(new singleItem("n","1000",""));
-        items.add(new singleItem("o","1000",""));
-        items.add(new singleItem("p","1000",""));
-        items.add(new singleItem("q","1000",""));
-        items.add(new singleItem("r","1000",""));
-        items.add(new singleItem("s","1000",""));
-        items.add(new singleItem("t","1000",""));
-        items.add(new singleItem("u","1000",""));
+        initView();
 
-        RecyclerView recyclerView = findViewById(R.id.recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-        ItemAdapter adapter = new ItemAdapter(items);
-        recyclerView.setAdapter(adapter);
+    void initView(){
+        viewPager2 = (ViewPager2) findViewById(R.id.viewPager);
+        adapter = new FragmentAdapter(this);
+        fragments.add(new onePlusOneFragment());
+        fragments.add(new twoPlusOneFragment());
+
+        adapter.fragmentArrayList = fragments;
+        viewPager2.setAdapter(adapter);
     }
 }
