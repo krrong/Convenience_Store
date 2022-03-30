dd5a8423960aee1bbf0a46f5d7fccbebe7108cfe
krrong <khk9664@naver.com>
2022-03-30 19:48:07 +0900

    프래그먼트에 리사이클러뷰 추가

--- app/src/main/java/com/example/convenience_stores/twoPlusOneFragment.java
+++ app/src/main/java/com/example/convenience_stores/twoPlusOneFragment.java
@@ -3,62 +3,33 @@ package com.example.convenience_stores;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link twoPlusOneFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class twoPlusOneFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public twoPlusOneFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment twoPlusOneFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static twoPlusOneFragment newInstance(String param1, String param2) {
-        twoPlusOneFragment fragment = new twoPlusOneFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private RecyclerView recyclerView;
+    private ItemAdapter adapter = new ItemAdapter(new ArrayList<singleItem>());
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two_plus_one, container, false);
+        ViewGroup viewGroup = (ViewGroup)inflater.inflate(R.layout.fragment_two_plus_one, container, false);
+
+        adapter.addItem(new singleItem("a", "1000", "http://bgf-cu.xcache.kinxcdn.com/product/6921211104292.jpg"));
+        adapter.addItem(new singleItem("b", "2000", "http://bgf-cu.xcache.kinxcdn.com/product/6921211104292.jpg"));
+        adapter.addItem(new singleItem("C", "3000", "http://bgf-cu.xcache.kinxcdn.com/product/6921211104292.jpg"));
+
+        recyclerView = (RecyclerView)viewGroup.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        return viewGroup;
     }
 }
