22bbe3311c0bf235cc6bb12699b6e3b067ee23fe
krrong <khk9664@naver.com>
2022-04-30 17:59:13 +0900

    Update twoPlusOneFragment.java
    txt파일로부터 데이터를 파싱하는 dataParsing() 함수 삭제
    
    Goods 액티비티에서 Bundle로 데이터를 넘겨 받는 getData() 함수 생성

--- app/src/main/java/com/example/convenience_stores/twoPlusOneFragment.java
+++ app/src/main/java/com/example/convenience_stores/twoPlusOneFragment.java
@@ -30,7 +30,7 @@ public class twoPlusOneFragment extends Fragment {
         // Inflate the layout for this fragment
         ViewGroup viewGroup = (ViewGroup)inflater.inflate(R.layout.fragment_two_plus_one, container, false);
 
-        dataParsing();  // 데이터 파싱
+        getData();
         dataLoad();     // 데이터 로드
 
         // 리사이클러뷰 바인딩 및 어댑터와 연결
@@ -41,59 +41,12 @@ public class twoPlusOneFragment extends Fragment {
         return viewGroup;
     }
 
-    private void dataParsing(){
-        // Goods.java 에서 전달한 번들 저장
+    // Goods 액티비티에서 Bundle 객체를 이용하여 데이터를 받아오는 함수
+    private void getData(){
         Bundle bundle = getArguments();
-
-        // 번들 안의 텍스트 불러오기
-        String place = bundle.getString("place");
-
-        InputStream inName;
-        InputStream inPrice;
-        InputStream inUrl;
-
-        // 편의점과 행사에 맞는 파일 가져오기
-        // 2+1
-        if (place.equals("CU")){
-            inName = getResources().openRawResource(R.raw.cu_21_name);
-            inPrice = getResources().openRawResource(R.raw.cu_21_price);
-            inUrl  = getResources().openRawResource(R.raw.cu_21_link);
-        }
-        else if(place.equals("7ELEVEn")){
-            inName = getResources().openRawResource(R.raw.seven_21_name);
-            inPrice = getResources().openRawResource(R.raw.seven_21_price);
-            inUrl  = getResources().openRawResource(R.raw.seven_21_link);
-        }
-        else{
-            inName = getResources().openRawResource(R.raw.gs_21_name);
-            inPrice = getResources().openRawResource(R.raw.gs_21_price);
-            inUrl  = getResources().openRawResource(R.raw.gs_21_link);
-        }
-
-        // 파일에서 데이터 읽어오기
-        try {
-            byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
-            byte[] bPrice = new byte[inPrice.available()];
-            byte[] bUrl = new byte[inUrl.available()];
-
-            // 인자만큼 읽어오기
-            inName.read(bName);
-            inPrice.read(bPrice);
-            inUrl.read(bUrl);
-
-            // byte -> string 변환
-            String s_name = new String(bName);
-            String s_price = new String(bPrice);
-            String s_url = new String(bUrl);
-
-            // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
-            nameList = s_name.split("\n");
-            priceList = s_price.split("\n");
-            urlList = s_url.split("\n");
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+        nameList = bundle.getStringArray("nameList");
+        priceList = bundle.getStringArray("priceList");
+        urlList = bundle.getStringArray("urlList");
     }
 
     // 데이터 로드 (전체 데이터 한번에 로드)
