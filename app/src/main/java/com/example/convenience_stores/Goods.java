78525f4514fadf643f722239875da3c444d7ec90
krrong <khk9664@naver.com>
2022-05-11 12:51:37 +0900

    번들 객체 이용 방법 삭제, dataParsing()함수 수정
    1) 번들 객체 이용 방법 삭제
    번들 객체를 이용하여 데이터를 넘겨주는 방법 삭제하고 뷰페이저로부터 FrameAdapter를 받아오고 FrameAdapter로부터 Fragment를 받아오고 Fragment의 함수를 이용하여 데이터를 수정하는 방법을 사용했다.
    
    2) dataParsing()함수 수정
    인자로 position을 같이 넘겨주어 현재 보고 있는 프래그먼트를 뷰페이저의 어댑터로부터 찾을 수 있도록 하였다.
    
    3) 뷰페이저 어댑터(FrameAdapter), 프래그먼트를 담을 리스트 삭제
    전역으로 선언했던 뷰페이저 어댑터(FrameAdapter)를 삭제하고 지역으로 선언하여 사용하도록 수정했다.
    
    전역으로 선언했던 프래그먼트를 담는 리스트를 삭제하고 지역으로 재선언하여 사용했다.

--- app/src/main/java/com/example/convenience_stores/Goods.java
+++ app/src/main/java/com/example/convenience_stores/Goods.java
@@ -29,12 +29,11 @@ public class Goods extends FragmentActivity {
     String[] priceList;
     String[] urlList;
     Bundle bundle = new Bundle();                               // viewPager2와 Fragment가 정보를 주고 받는 매개체
-    boolean flag = true;                                       //
 
     private ViewPager2 viewPager2;                              // 뷰페이저
     private TabLayout tabLayout;                                // 레이아웃
-    private FragmentAdapter adapter;                            // 뷰페이저 어댑터
-    private ArrayList<Fragment> fragments = new ArrayList<Fragment>(); // 1+1, 2+1 프래그먼트 담을 ArrayList
+    //private FragmentAdapter adapter;                            // 뷰페이저 어댑터
+    //private ArrayList<Fragment> fragments = new ArrayList<Fragment>(); // 1+1, 2+1 프래그먼트 담을 ArrayList
     private ArrayList<String> tabTitles = new ArrayList<String>();   // 1+1, 2+1 탭 타이틀 담을 ArrayList
 
     @Override
@@ -56,36 +55,35 @@ public class Goods extends FragmentActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         searchConvenience = (Button) findViewById(R.id.searchConvenience);
         searchGoods = (Button) findViewById(R.id.searchGoods);
-        adapter = new FragmentAdapter(this);
 
+        ArrayList<Fragment> fragments = new ArrayList();
+
+        // fragment 생성
+        fragments.add(new onePlusOneFragment());    // 1+1 프래그먼트
+        fragments.add(new twoPlusOneFragment());    // 2+1 프래그먼트
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(this, fragments);
+
+        // 주변 편의점 검색 버튼 텍스트
         searchConvenience.setText("주변 " + place + "검색하기");
 
         // 번들 객체 생성
         bundle.putString("place", place);
 
-        // fragment 생성
-        fragments.add(new onePlusOneFragment());
-        fragments.add(new twoPlusOneFragment());
-        
-        // fragment로 bundle 전달
-        fragments.get(0).setArguments(bundle);
-        fragments.get(1).setArguments(bundle);
-
         // 탭 타이틀 추가
         tabTitles.add("1+1");
         tabTitles.add("2+1");
 
-        adapter.fragmentArrayList = fragments;
+        // fragmentAdapter에 fragment 넣어주기
+        fragmentAdapter.fragmentArrayList = fragments;
 
         // 뷰페이저에 어댑터 연결
-        viewPager2.setAdapter(adapter);
+        viewPager2.setAdapter(fragmentAdapter);
         
         // 현재 보고 있는 Fragment를 얻기 위한 콜백 생성
         viewPager2.registerOnPageChangeCallback(viewPagerCallback);
 
-        // 처음에 1+1 Fragment에 데이터를 넘겨주기 위해 실행
-        dataParsing(place, "1+1");
-
+        // 탭 타이틀 연결
         new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull @NotNull TabLayout.Tab tab, int position) {
@@ -94,11 +92,11 @@ public class Goods extends FragmentActivity {
                 tab.setCustomView(textView);
             }
         }).attach();
-
     }
 
     // 리스너 함수 bind
     private void setListener(){
+        // 편의점 검색 버튼 클릭 함수
         searchConvenience.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -107,11 +105,11 @@ public class Goods extends FragmentActivity {
                 startActivity(intent);
             }
         });
-
+        // 상품 검색 버튼 클릭 함수
         searchGoods.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), test.class);
+                Intent intent = new Intent(getApplicationContext(), SearchGoodActivity.class);
                 intent.putExtra("place", place);
                 intent.putExtra("nameList", nameList);
                 intent.putExtra("priceList", priceList);
@@ -124,22 +122,28 @@ public class Goods extends FragmentActivity {
     // 2+1 Fragment에 데이터를 넘겨주기 위해 콜백으로 작성(Fragment 전환 시 실행)
     private ViewPager2.OnPageChangeCallback viewPagerCallback = new ViewPager2.OnPageChangeCallback() {
         @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+        }
+
+        @Override
         public void onPageSelected(int position) {
             super.onPageSelected(position);
 
             // 1+1 Fragment : 1+1파일 파싱해서 보내주기
             if (position == 0){
-                dataParsing(place, "1+1");
+                dataParsing(position, place, "1+1");
             }
             // 2+1 Fragment : 2+1파일 파싱해서 보내주기
             else if(position == 1){
-                dataParsing(place, "2+1");
+                dataParsing(position, place, "2+1");
             }
+            Log.e("onPageSelected : ", Integer.toString(position));
         }
     };
 
-    // place, event에 맞는 데이터를 파싱하여 Fragment에 전달
-    private void dataParsing(String place, String event){
+    // place, event에 맞는 데이터를 파싱하여 position에 위치한 Fragment에 전달
+    private void dataParsing(int position, String place, String event){
         InputStream inName;
         InputStream inPrice;
         InputStream inUrl;
@@ -203,12 +207,17 @@ public class Goods extends FragmentActivity {
             priceList = s_price.split("\n");
             urlList = s_url.split("\n");
 
-            bundle.putStringArray("nameList", nameList);
-            bundle.putStringArray("priceList", priceList);
-            bundle.putStringArray("urlList", urlList);
+            // fragment에 넘겨줄 데이터를 items에 담음
+            ArrayList<singleItem> items = new ArrayList<>();
+            for(int i=0; i<nameList.length; i++){
+                items.add(new singleItem(nameList[i], priceList[i], urlList[i]));
+            }
 
+            // 각 fragment에 알맞는 값 넘겨주기
+            FragmentAdapter adapter = (FragmentAdapter) viewPager2.getAdapter();
+            GoodBaseFragment fragment = (GoodBaseFragment) adapter.getItem(position);
+            fragment.setData(items);
         }catch (Exception e){
-            e.printStackTrace();
         }
     }
 }
