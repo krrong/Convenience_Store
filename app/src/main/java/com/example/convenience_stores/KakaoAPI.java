29d0defa671a19915fd9ce04ada2b6cc5331f6a0
krrong <khk9664@naver.com>
2022-05-21 15:36:55 +0900

    주석 수정

--- app/src/main/java/com/example/convenience_stores/KakaoAPI.java
+++ app/src/main/java/com/example/convenience_stores/KakaoAPI.java
@@ -8,10 +8,10 @@ import retrofit2.http.Query;
 public interface KakaoAPI {
     @GET("v2/local/search/keyword.json")    // Keyword.json 의 정보를 받아옴
 
-        // 받아온 정보가 ResultSearchKeyword 클래스의 구조로 담김
+    // 받아온 정보가 ResultSearchKeyword 클래스의 구조로 담김
     Call<ResultSearchKeyword> getSearchKeyword(
             @Header("Authorization") String key,    // 카카오 API 인증키
-            @Query("query") String query,          // 검색을 원하는 질의어
+            @Query("query") String query,           // 검색을 원하는 질의어
             @Query("x") String x,                   // longitude
             @Query("y") String y,                   // latitude
             @Query("radius") int radius);           // 반경거리
