3dabf74f4d80e42b4ef960e7172a9c4e42228edd
krrong <khk9664@naver.com>
2022-06-19 02:31:14 +0900

    json형식 파일 생성하도록 수정
    수정전 : GoodBaseFragment에서 상품의 이름, 가격, url이 저장되어 있는 파일을 편의점의 종류와 선택된 이벤트에 따라 다르게 가져오도록 했음
    
    수정후 : MainActivity에서 내부 저장소에 편의점의 종류와 이벤트에 따라 상품의 이름, 가격, url을 JSON형식으로 관리하도록 파일 생성 (정상적으로 동작한다면 내부 저장소에 총 6개의 파일이 생성됨)
    
    TODO : GoodBaseFragment에서 내부 저장소에 저장된 파일 중 선택된 편의점과 이벤트에 따라 알맞는 파일을 가져와 사용하도록 수정해야함

--- app/src/main/java/com/example/convenience_stores/activity/MainActivity.java
+++ app/src/main/java/com/example/convenience_stores/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.convenience_stores.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -14,9 +15,26 @@ import android.widget.Button;
 
 import com.example.convenience_stores.R;
 import com.example.convenience_stores.activity.GoodsActivity;
+import com.example.convenience_stores.data.SingleItem;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Button cu_btn;
@@ -30,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
         setBtnListener();
+        makeJson();
     }
 
     private void initView(){
@@ -88,6 +107,113 @@ public class MainActivity extends AppCompatActivity {
             }catch(NoSuchAlgorithmException e){
                 e.printStackTrace();
             }
+        }
+    }
+
+    // JSON 파일 형식으로 변경
+    private void makeJson() {
+        int nameFiles[] = {R.raw.cu_11_name, R.raw.seven_11_name, R.raw.gs_11_name, R.raw.cu_21_name, R.raw.seven_21_name, R.raw.gs_21_name};
+        int priceFiles[] = {R.raw.cu_11_price, R.raw.seven_11_price, R.raw.gs_11_price, R.raw.cu_21_price, R.raw.seven_21_price, R.raw.gs_21_price};
+        int urlFiles[] = {R.raw.cu_11_link, R.raw.seven_11_link, R.raw.gs_11_link, R.raw.cu_21_link, R.raw.seven_21_link, R.raw.gs_21_link};
+        String names[] = {"cu11.txt", "seven11.txt", "gs11.txt", "cu12.txt", "seven12.txt", "gs12.txt"};
+
+        for(int i=0;i<nameFiles.length; i++){
+            String saveFileName = names[i];
+            InputStream inName = getResources().openRawResource(nameFiles[i]);
+            InputStream inPrice = getResources().openRawResource(priceFiles[i]);
+            InputStream inUrl = getResources().openRawResource(urlFiles[i]);
+
+            JSONObject jsonObject = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            // 파일에서 데이터 읽어오기
+            try {
+                byte[] bName = new byte[inName.available()]; // available = 읽을 수 있는 바이트 수 반환
+                byte[] bPrice = new byte[inPrice.available()];
+                byte[] bUrl = new byte[inUrl.available()];
+
+                // 인자만큼 읽어오기
+                inName.read(bName);
+                inPrice.read(bPrice);
+                inUrl.read(bUrl);
+
+                // byte -> string 변환
+                String sName = new String(bName);
+                String sPrice = new String(bPrice);
+                String sUrl = new String(bUrl);
+
+                // "\n" 단위로 나누어 상품명, 가격, 이미지 url 배열에 저장
+                String[] nameList = sName.split("\n");
+                String[] priceList = sPrice.split("\n");
+                String[] urlList = sUrl.split("\n");
+
+                // JSONObject로 변경 후 내부 저장소 파일에 write
+                for(int j=0; j< nameList.length; j++){
+                    // 하나의 오브젝트로 사용하려다보니 같은 값이 반복되어 들어가서 새로 재생성
+                    jsonObject = new JSONObject();
+
+                    jsonObject.put("name", nameList[j]);
+                    jsonObject.put("price", priceList[j]);
+                    jsonObject.put("url", urlList[j]);
+
+                    jsonArray.put(jsonObject);
+                }
+                wrtieFile(saveFileName, jsonArray);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 내부 저장소에 파일 쓰기
+    void wrtieFile(String fileName, JSONArray msg){
+        // Context.MODE_PRIVATE : 덮어쓰기
+        // Context.MODE_APPEND : 이어쓰기
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE));
+
+            for(int i=0;i<msg.length();i++){
+                outputStreamWriter.write(msg.getJSONObject(i).toString());
+                outputStreamWriter.write("\n");
+            }
+            outputStreamWriter.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 내부 저장소에 저장된 파일 읽기
+    void readFile(String fileName) {
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = null;
+
+        try {
+            InputStream inputStream = openFileInput(fileName);
+            if(inputStream != null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String data = bufferedReader.readLine();
+
+                // 파일 전체 읽기
+                while(data != null){
+                    Object obj = parser.parse(data);
+                    jsonObject = (JsonObject) obj;
+                }
+
+                Log.e("HIHI", jsonObject.toString());
+
+                inputStream.close();
+                inputStreamReader.close();
+                bufferedReader.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
